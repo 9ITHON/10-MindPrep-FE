01c31e6aef2aa8dfaa71866232d5a8a26d0b0437
JaeSunEo <wotjs020708@gmail.com>
2025-07-10 16:09:42 +0900

    Initial Commit

---
+++ MindPrep/MindPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPrepApp.swift
+//  MindPrep
+//
+//  Created by 어재선 on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MindPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
