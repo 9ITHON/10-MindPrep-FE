7680063e6265e2cefc07f521076e53b2d73c2709
JaeSunEo <wotjs020708@gmail.com>
2025-07-12 01:03:48 +0900

    fix: UIkit Code -> SwiftUI Code

---
+++ MindPrep/Component/NavigationBar/CustomNavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationBar.swift
+//  MindPrep
+//
+//  Created by 어재선 on 7/12/25.
+//
+
+import SwiftUI
+
+struct CustomNavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationBar()
+}
---
+++ MindPrep/Component/NavigationBar/NavigationBtnType.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationBtnType.swift
+//  MindPrep
+//
+//  Created by 어재선 on 7/12/25.
+//
+
+import Foundation
